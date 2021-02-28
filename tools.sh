d00fd1cf1a9e624eefc9b70588c2b69a72a6afb7
9unknown9 <79751788+9unknown9@users.noreply.github.com>
2021-02-28 17:07:27 +0700

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -1,123 +1,109 @@
 clear
+
 blue='\033[34;1m'
+
 green='\033[32;1m'
+
 purple='\033[35;1m'
+
 cyan='\033[36;1m'
+
 red='\033[31;1m'
+
 white='\033[37;1m'
+
 yellow='\033[33;1m'
+
 sleep 1
+
 echo
+
 echo $green"================================================="
+
 echo $blue"by"
+
 toilet -f big -F gay    sks_mint
-echo
-echo $green"================================================="             
-echo
-echo $green"                  SelamatDatang  "
-echo
-echo $yellow "             Silahkan Pilih Tools :   "
-echo $green"--------------------------------------------------"
-echo
-echo $yellow"1.)Stabilkan Jaringan"
-echo $green"2.)Spam Whatsapp"
-echo $blue"3.)spam sms"
-echo $red"4.)keluar"
-echo
-echo $white
-read -p "PilihMana : " Bro
 
-if [ $Bro = 1 ] || [ $Bro = 1 ]
-then
-clear
-toilet -f big -F gay sks_mint
-echo $green"Stabilkan Jaringan by sks_mint"
-sleep 2
-ping -s1000  1.1.1.1
-fi
-
-if [ $Bro = 2 ] || [ $Bro = 2 ]
-then
-clear
-echo $blue"tunggu bentar"
-cd Brutal-Wa
-sleep 2
-bash install.sh
-fi
-
-if [ $Bro = 3 ] || [ $Bro = 3 ]
-then
-clear
-echo $yellow"tunngu bentar"
-sleep 2
-python sms.py
-exit
-fi
-
-if [ $Bro = 4 ] || [ $Bro = 4 ]
-then
-clear
-sleep 2
-
-
-
-clear
-blue='\033[34;1m'
-green='\033[32;1m'
-purple='\033[35;1m'
-cyan='\033[36;1m'
-red='\033[31;1m'
-white='\033[37;1m'
-yellow='\033[33;1m'
-sleep 1
 echo
-echo $green"================================================="
-echo $blue"by"
-toilet -f big -F gay    sks_mint
-echo
+
 echo $green"================================================="       >
+
 echo
+
 echo $green"                  SelamatDatang  "
+
 echo
+
 echo $yellow "             Silahkan Pilih Tools :   "
+
 echo $green"--------------------------------------------------"
+
 echo
+
 echo $yellow"1.)Stabilkan Jaringan"
+
 echo $green"2.)Spam Whatsapp"
+
 echo $blue"3.)spam sms"
+
 echo $red"4.)keluar"
+
 echo
+
 echo $white
+
 read -p "PilihMana : " Bro
 
 if [ $Bro = 1 ] || [ $Bro = 1 ]
+
 then
+
 clear
+
 toilet -f big -F gay sks_mint
+
 echo $green"Stabilkan Jaringan by sks_mint"
+
 sleep 2
+
 ping -s1000  1.1.1.1
+
 fi
 
 if [ $Bro = 2 ] || [ $Bro = 2 ]
+
 then
+
 clear
+
 echo $blue"tunggu bentar"
+
 git clone https://github.com/Sxp-ID/Brutal-Wa
+
 cd Brutal-Wa
+
 sleep 2
+
 bash install.sh
+
 fi
 
 if [ $Bro = 3 ] || [ $Bro = 3 ]
-then
-clear
-echo $yellow"tunngu bentar"
-sleep 2
-git clone https://github.com/9unknown9/spam.sms
-cd spam.sms
-python sms.py
 
+then
+
+clear
+
+echo $yellow"tunngu bentar"
+
+sleep 2
+
+git clone https://github.com/9unknown9/spam.sms
+
+cd spam.sms
+
+python sms.py
 
 exit
 fi
+
